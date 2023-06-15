3b2bd1f4ea593f680aa65194d658ecd1d8adc1f9
Vitoria Damasceno <83039172+vitoriadaamasceno@users.noreply.github.com>
2023-06-15 14:49:09 -0300

    Update Main.java

--- Sobrecarga e Poliformismo/interfaces/Main.java
+++ Sobrecarga e Poliformismo/interfaces/Main.java
@@ -6,10 +6,9 @@ public class Main {
 
         Cachorro dog = new Cachorro();
         Gato cat = new Gato();
-        PitbullEnraivado pit = new PitbullEnraivado();
 
         System.out.println("Cachorro: " + dog.emitirSom() + "- Ele come: " + dog.comer());
         System.out.println("Gato: " + cat.emitirSom() + "- Ele come: " + cat.comer());
-        System.out.println("Pitbull Enraivado: " + pit.emitirSom() + "- Ele come: " + pit.comer());
+
     }
 }
